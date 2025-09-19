376e5011a466ca7dee7ce5a5c4421d8920824b59
김재원 <wodnjs9058@naver.com>
2025-09-19 14:19:49 +0900

    inital commit

---
+++ src/main/java/com/handling/api/multi/multiAPIHandling/MultiApiHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.handling.api.multi.multiAPIHandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiApiHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiApiHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/handling/api/multi/multiAPIHandling/MultiApiHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handling.api.multi.multiAPIHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiApiHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
